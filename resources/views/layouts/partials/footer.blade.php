f55b218abe69520efaf3bb2b0d55481b5cf78cb0
devhimelali <dev.himelali@gmail.com>
2026-03-04 03:24:21 +0600

    Add theme customizer script to enable personalized layout and color settings

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="copyright-footer d-flex align-items-center justify-content-between border-top bg-white gap-3 flex-wrap">
+    <p class="fs-13 text-gray-9 mb-0">{{date('Y')-1}} - {{date('Y')}} &copy; {{config('app.name')}}. All Right Reserved</p>
+    <p>Designed & Developed By <a href="https://devhimel.com" class="link-primary">Md. Himel Ali</a></p>
+</div>
